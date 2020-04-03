cf501fb0a8c99c80d3dabe231114f6f1becdfda9
Javi <javipariente@gmail.com>
2020-04-03 00:56:03 -0600

    playing with forms

--- src/MyForm.js
+++ src/MyForm.js
@@ -5,7 +5,12 @@ class MyForm extends React.Component {
 
   constructor(props){
     super(props);
-    this.state = {username: "", age:""};
+    this.state = {
+      username: "", 
+      age:"", 
+      textMessage: "",
+      countrySelected: "Canada", 
+      errorMessage: ""};
   }
 
   inputChangeHandler = (event) => {
@@ -15,32 +20,50 @@ class MyForm extends React.Component {
     let inputValue = event.target.value;
 
     if (inputName === "age" && isNaN(inputValue)){
-      alert("Age is not a numeric value !");
-      event.target.value = "";    }
+      this.setState({errorMessage:"Your age value must be a number before submit"});
+      document.getElementById("btnSubmit").disabled = true;
+      //event.target.value = "";    
+    }
     else{
       this.setState({[event.target.name] : event.target.value});
+      this.setState({errorMessage:""});
+      document.getElementById("btnSubmit").disabled = false;
     }
   }
 
   inputSubmitHandler = (event) => {
     event.preventDefault();
-    alert("Submitting for: " + this.state.username + ",who is: " + this.state.age);
+    alert("Submitting for: " + this.state.username + ",who is: " + this.state.age + 
+          " .Notes: " + this.state.textMessage + ". Country:" + this.state.countrySelected);
 
   }
 
   render(){
 
         let header = (this.state.username.length > 0) ? <h1>Hello {this.state.username}</h1> : "";
+        let errorMessage = this.state.errorMessage;
 
         return (
+            <div>
             <form>
             {header}
             <p>Enter your name:</p>
             <input type="text" name='username' onChange={this.inputChangeHandler} />
             <p>Enter your age:</p>
             <input type='text' name='age' onChange={this.inputChangeHandler} />
-            <input type="button" value="Submit Form" onClick={this.inputSubmitHandler} />
+            <p>Notes:</p>
+            <input type='textarea' name='textMessage' onChange={this.inputChangeHandler} />
+            <p>Country of residence:</p>
+            <select value={this.state.countrySelected} name="countrySelected" onChange={this.inputChangeHandler} >
+                <option value="Spain">Spain</option>
+                <option value="USA">USA</option>
+                <option value="Canada">Canada</option>
+            </select>
+            <input id="btnSubmit" type="button" value="Submit Form" onClick={this.inputSubmitHandler} />
             </form>
+            {errorMessage}
+            </div>
+            
         );
     } 
   }
